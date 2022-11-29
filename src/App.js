27d542bcd16e7fb2bd8dcf1685bfb12f73e40d0b
David Wang <david2000wang@gmail.com>
2022-11-28 23:57:11 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+
+/* Todos:
+- CRUD (delete)
+- time
+- enter keyboard press
+*/
 function App() {
+  const [toDos, setToDos] = useState([])
+  const [isemptyText, setIsEmptyText] = useState(true)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Todo app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* add new todos */}
+        <form>
+          <label htmlFor = "todoInput">Add Todo</label>
+          <input type="text" id="todoInput" onChange={() => {
+            document.getElementById("todoInput").value == "" ? setIsEmptyText(true): setIsEmptyText(false)}
+            }/>
+          <input type="button" value="Submit" disabled={isemptyText} onClick={() => setToDos(current => [...current, document.getElementById("todoInput").value])}/>
+        </form>
+
+        {/* display todos */}
+        <div className="toDos" id="toDos">
+          {toDos.map((todo, index) => (
+            <li key={index}>
+              <span>{todo}</span>
+            </li>
+          ))}
+        </div>
       </header>
+      <div>
+        <a
+            className="App-link"
+            href="https://davidlwang.xyz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My website
+          </a>
+      </div>
     </div>
   );
 }
